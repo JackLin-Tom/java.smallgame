3263f6c3296335f6c0220c5da8a92da0f7f9890f
justnowlin <2253805775@qq.com>
2022-03-20 00:09:49 +0800

    随机生成敌方坦克

---
+++ src/com/sxt/Bot.java
@@ -0,0 +1,24 @@
+package com.sxt;
+
+import java.awt.*;
+
+public class Bot extends Tank {
+
+
+    public Bot(String img, int x, int y, GamePanel gamePanel, String upImg, String leftImg, String rightImg, String downImg) {
+        super(img, x, y, gamePanel, upImg, leftImg, rightImg, downImg);
+    }
+
+
+    @Override
+    public void paintSelft(Graphics g) {
+        g.drawImage(img,x,y,null);
+
+
+    }
+
+    @Override
+    public Rectangle getRec() {
+        return new Rectangle(x,y,width,height);
+    }
+}
